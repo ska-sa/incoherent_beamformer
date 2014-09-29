7df265074545e7d0fbd5d81da53fc45d01da569a
Christopher Schollar <ctgschollar@gmail.com>
2014-09-29 15:06:00 +0200

    SR-5 Updated spead callback code. If speadrx is run with the to_buffer callback then the spead threads will save the data to a dada_buffer in the correct location in that buffer determined by the difference between timestamp of the incoming heap and the first timestamp for the buffer which is being written to. It is important to set the NUM_WORKERS in the to_buffer.c code to the number of workers speadrx is run with, otherwise the thread synchronization will fail.

--- spead_callback/to_buffer.c
+++ spead_callback/to_buffer.c
@@ -5,230 +5,189 @@
 #include <string.h>
 #include <fcntl.h>
 
+#include <dada_buffer.h>
+
 #include <spead_api.h>
 
 #define F_ID_SPEAD_ID 4096
 #define TIMESTAMP_SPEAD_ID 4097
 #define DATA_SPEAD_ID 4098
-#define DEFAULT_PREFIX "/data1/latest"
-#define DEFAULT_FILENAME "spead_out"
-#define FILE_HEAP_LEN 8000
+#define DATA_LEN 2048
+#define DADA_BUF_ID 0x1234
+#define DADA_NUM_READERS 1
+#define DADA_NUM_BUFS 16
+#define NUM_HEAPS_PER_BUFFER 100
+
+
+#define TS_PER_HEAP 1
+#define CHANS_PER_HEAP 1024
+#define POLARIZATIONS 2
+#define BYTES_PER_SAMPLE 1
+#define EXPECTED_HEAP_LEN TS_PER_HEAP * CHANS_PER_HEAP * POLARIZATIONS * BYTES_PER_SAMPLE
+
+#define DADA_BUFFER_SIZE EXPECTED_HEAP_LEN * NUM_HEAPS_PER_BUFFER
+
+#define NUM_WORKERS 4
 
 typedef enum { false, true } bool;
 
-int w_fd2;
-unsigned long long heap_cnt;
-int file_count;
+char* buffer; //local pointer to current buffer
+unsigned long long heap_count;
+unsigned long prior_ts; //local prior ts
 
-//FROM THE PUMA2_DMADB MAIN METHOD
-
-/* header */
-char *header = default_header;
-char *header_buf = 0;
-unsigned header_strlen = 0;/* the size of the header string */
-uint64_t header_size = 0;/* size of the header buffer */
-char writemode = 'w';
-
-/* DADA Header plus Data Unit */
-dada_hdu_t *hdu;
-
-/* primary write client main loop  */
-dada_pwc_main_t *pwcm;
-
-dmadb_t dmadb;
-
-time_t utc;
-
-unsigned buffer_size = 64;
-
-static char *buffer = 0;
-
-/* flags */
-char daemon = 0; /*daemon mode */
-
-int verbose = 0;/* verbose mode */
-
-int mode = 0;
-
-//-----------------------------------------
 
 struct snap_shot
 {
-    unsigned long long prior_ts;
-    char filename[255];
-    char *prefix;
-    unsigned long long offset;
-    unsigned long long data_len;
-    int id;
-    bool with_markers;
-
-    int file_count;
     int master_id;
-    int new_file;
+    ipcbuf_t circular_buf;
+    char* buffer; //global pointer to current buffer
+    unsigned long long prior_ts; //first ts for this buffer
+    int buffer_full;
 };
 
 void spead_api_destroy(struct spead_api_module_shared *s, void *data)
 {
+    struct snap_shot *ss;
+
+    lock_spead_api_module_shared(s);
+
+    if ((ss = get_data_spead_api_module_shared(s)) != NULL)
+    {
+        delete_buffer(&ss->circular_buf);
+
+        shared_free(ss, sizeof(struct snap_shot));
+
+        clear_data_spead_api_module_shared(s);
+#ifdef DEBUG
+        fprintf(stderr, "%s: PID [%d] destroyed spead_api_shared\n", __func__, getpid());
+#endif
+    }
+    else
+    {
+
+#ifdef DEBUG
+        fprintf(stderr, "%s: PID [%d] spead_api_shared is clean\n", __func__, getpid());
+#endif
+
+    }
+
+    printf("EXITING");
+    unlock_spead_api_module_shared(s);
 
 }
 
 void *spead_api_setup(struct spead_api_module_shared *s)
 {
+    struct snap_shot *ss;
 
-    /* Initialize DADA/PWC structures*/
-    pwcm = dada_pwc_main_create ();
+    ss = NULL;
 
-    pwcm->context = &dmadb;
+    lock_spead_api_module_shared(s);
 
-    pwcm->log = multilog_open ("to_buffer", daemon);
-
-    /* set up for daemon usage */
-    if (daemon)
+    if (!(ss = get_data_spead_api_module_shared(s)))
     {
-        be_a_daemon ();
-        multilog_serve (pwcm->log, DADA_DEFAULT_PWC_LOG);
-    }
-    else
-    {
-        //multilog_add (pwcm->log, stderr);
-        multilog_serve (pwcm->log, DADA_DEFAULT_PWC_LOG);
-    }
-
-    /* create the header/data blocks */
-
-    hdu = dada_hdu_create (pwcm->log);
-
-    if (dada_hdu_connect (hdu) < 0)
-        return EXIT_FAILURE;
-
-    /* make data buffers readable */
-
-    if (dada_hdu_lock_write_spec (hdu, writemode) < 0)
-        return EXIT_FAILURE;
-
-    /* if we are a stand alone application. Don't know what this means, but it comes from the puma2 code*/
-
-    if ( mode == 1)
-    {
-
-        header_size = ipcbuf_get_bufsz (hdu->header_block);
-        multilog (pwcm->log, LOG_INFO, "header block size = %llu\n", header_size);
-
-        header_buf = ipcbuf_get_next_write (hdu->header_block);
-
-        if (!header_buf)
+        ss = shared_malloc(sizeof(struct snap_shot));
+        if (ss == NULL)
         {
-            multilog (pwcm->log, LOG_ERR, "Could not get next header block\n");
-            return EXIT_FAILURE;
+            unlock_spead_api_module_shared(s);
+            return NULL;
         }
 
-        /* if header file is presented, use it. If not set command line attributes */
-        if (header_file)
-        {
-            if (fileread (header_file, header_buf, header_size) < 0)
-            {
-                multilog (pwcm->log, LOG_ERR, "Could not read header from %s\n", header_file);
-                return EXIT_FAILURE;
-            }
-        }
-        else
-        {
-            header_strlen = strlen(header);
-            memcpy (header_buf, header, header_strlen);
-            memset (header_buf + header_strlen, '\0', header_size - header_strlen);
-
-            if (ascii_header_set (header_buf, "HDR_SIZE", "%llu", header_size) < 0)
-            {
-                multilog (pwcm->log, LOG_ERR, "Could not write HDR_SIZE to header\n");
-                return -1;
-            }
-
-            if (verbose) fprintf(stderr, "Observation ID is %s\n", ObsId);
-            if (ascii_header_set (header_buf, "OBS_ID", "%s", ObsId) < 0)
-            {
-                multilog (pwcm->log, LOG_ERR, "Could not write OBS_ID to header\n");
-                return -1;
-            }
-
-            if (verbose) fprintf(stderr, "File size is %lu\n", fSize);
-            if (ascii_header_set (header_buf, "FILE_SIZE", "%lu", fSize) < 0)
-            {
-                multilog (pwcm->log, LOG_ERR, "Could not write FILE_SIZE to header\n");
-                return -1;
-            }
-        }
-
-        /* if number of buffers is not specified, */
-        /* find it from number of seconds, based on 80 MB/sec sample rate*/
-        if (nbufs == 0)
-        {
-            float tmp;
-            tmp = (float)((80000000 * nSecs) / BUFSIZE);
-            nbufs = (int) tmp;
-            if (verbose) fprintf(stderr, "Number of bufs is %d %f\n", nbufs, tmp);
-        }
-
-        /* write UTC_START to the header */
-        if (ascii_header_set (header_buf, "UTC_START", "%s", buffer) < 0)
-        {
-            multilog (pwcm->log, LOG_ERR, "failed ascii_header_set UTC_START\n");
-            return -1;
-        }
-
-        /* donot set header parameters anymore - acqn. doesn't start */
-        if (ipcbuf_mark_filled (hdu->header_block, header_size) < 0)
-        {
-            multilog (pwcm->log, LOG_ERR, "Could not mark filled header block\n");
-            return EXIT_FAILURE;
-        }
-
-        /* The write loop: repeat for n buffers */
-        while (dmadb.buf < nbufs - 1 )
-        {
-
-            uint64_t bsize = BUFSIZE;
-
-            src = (char *)get_next_buf(pwcm, &bsize);
-
-            /* write data to datablock */
-            if (( ipcio_write(hdu->data_block, src, BUFSIZE) ) < BUFSIZE )
-            {
-                multilog(pwcm->log, LOG_ERR, "Cannot write requested bytes to SHM\n");
-                return EXIT_FAILURE;
-            }
-
-            wrCount = ipcbuf_get_write_count ((ipcbuf_t *)hdu->data_block);
-            rdCount = ipcbuf_get_read_count ((ipcbuf_t *)hdu->data_block);
-            if (verbose) fprintf(stderr, "%d %d %d\n", dmadb.buf, wrCount, rdCount);
-
-        }
-
-        if ( stop_acq(pwcm) != 0)
-            fprintf(stderr, "Error stopping acquisition");
-
-        if (dada_hdu_unlock_write (hdu) < 0)
-            return EXIT_FAILURE;
-
-        if (dada_hdu_disconnect (hdu) < 0)
-            return EXIT_FAILURE;
-
+        create_buffer(ss->circular_buf, DADA_BUF_ID, DADA_NUM_READERS, DADA_NUM_BUFS, DADA_BUFFER_SIZE);
+        ss->dada_key = DADA_BUF_ID;
+        ss->buffer_full = 0;
+        ss->prior_ts = 0;
+        ss->master_pid = 0;
+        set_data_spead_api_module_shared(s, ss, sizeof(struct snap_shot));
     }
 
-    return NULL;
+    unlock_spead_api_module_shared(s);
 }
 
 
 int spead_api_callback(struct spead_api_module_shared *s, struct spead_item_group *ig, void *data)
 {
+    struct snap_shot *ss;
+    struct spead_api_item *itm;
+    unsigned long long ts;
+    unsigned long long offset;
+
+    ss = get_data_spead_api_module_shared(s); //Get shared data module
+    itm = NULL;
+
+    itm = get_spead_item_with_id(ig, TIMESTAMP_SPEAD_ID);
+
+    if (itm == NULL){
+    fprintf(stderr, "%s: No timestamp data found (id: 0x%x)\n", __func__, SPEAD_TS_ID);
+    return -1;
+    }
+    // TS is 5 byte unsigned 
+    ts = (int)itm->i_data[0] + (int)itm->i_data[1] * 256 + (int)itm->i_data[2] * 256 * 256 + (int)itm->i_data[3] * 256 * 256 * 256 + (int)itm->i_data[4] * 256 * 256 * 256 * 256;
+
+    itm = get_spead_item_with_id(ig, DATA_SPEAD_ID);
+    if (itm == NULL){
+        fprintf(stderr, "%s: No beamformer payload data found.\n", __func__); 
+        return -1;
+    }
+    // check that the heap size matches our expectations
+    if (itm->i_data_len != EXPECTED_HEAP_LEN) {
+       fprintf(stderr,"%s: Expecting heap size of %i, got %i\n",__func__,EXPECTED_HEAP_LEN, itm->i_data_len);
+       return -1;
+    }
+
+    lock_spead_api_module_shared(s); //lock shared data
+
+    if (ss == NULL)
+    {
+        fprintf(stderr, "ss is null");
+        unlock_spead_api_module_shared(s);
+        //Shared resources empty, unlock and return
+        return -1;
+    }
+
+    if (ss->prior_ts == 0) { //First thread to run
+        if (ss->master_pid == 0){
+            ss->master_pid = getpid();
+            // make sure we know who is boss
+            ss->prior_ts = ts;
+            ss->buffer=get_next_buf(ss->circular_buf);
+            fprintf(stderr,"Reset prior_ts base to %llu. Master pid set to %d\n. Created first buffer",ts,ss->master_pid);
+            set_data_spead_api_module_shared(s, ss, sizeof(struct snap_shot));
+        }
+    }
+
+    if (buffer == NULL) //Must be first time non master thread has run
+        buffer = ss->buffer;
+
+    offset = (ts - prior_ts) * EXPECTED_HEAP_LEN;
+
+    if (offset >= DADA_BUFFER_SIZE){ //Need to go to next buffer
+        if(ss->buffer_full == 0){ //First thread to need next buffer
+            fprintf(stderr,"Trigger new buffer...(%d)\n",getpid());
+            ss->buffer=get_next_buf(ss->circular_buf);
+            ss->prior_ts += NUM_HEAPS_PER_BUFFER;
+        }
+        ss->buffer_full++;
+        if (ss->buffer_full == NUM_WORKERS) //Last thread to need next buffer
+        {
+            fprintf(stderr,"Mark old buffer as full...(%d)\n",getpid());
+            mark_filled(ss->circular_buf, DADA_BUFFER_SIZE);
+            ss->buffer_full = 0;
+        }
+
+        set_data_spead_api_module_shared(s, ss, sizeof(struct snap_shot));
+
+        buffer = ss->buffer;
+        prior_ts = ss->prior_ts;
+        offset = (ts - prior_ts) * EXPECTED_HEAP_LEN;
+    }
+
+    unlock_spead_api_module_shared(s);
+
+    memcpy(buffer + offset, itm, EXPECTED_HEAP_LEN);  //Copy data to buffer
 
     return 0;
 }
-
-int write_data(void *data, uint64_t data_len, unsigned long offset)
-{
-
-    return 0;
-} // end of write_bf_data
 
 int spead_api_timer_callback(struct spead_api_module_shared *s, void *data)
 {
